bfdfbd57676207adc81c7257cbc38d765475b5c8
Chandran Goodchild <ch.goodchild@gmail.com>
2017-06-01 23:05:25 +0200

    Backing up again before I delete the comments for debugging

--- cg248/w05/subsequence/src/subsequence/Main.java
+++ cg248/w05/subsequence/src/subsequence/Main.java
@@ -28,32 +28,30 @@ public class Main {
 	int n = 0;
 	int m = 0;
 
-	System.out.println("Look for " + s2 + " in " + s1);
+	// System.out.println("Look for " + s2 + " in " + s1);
 	// Search for s2 in s1.
-	// for (; m < s2.length(); m++) {
-	    System.out.println("outer_cycle");
-	    while ((n < s1.length()) && (m < s2.length())) {
-		System.out.print(" m: " + m + " n: " + n);
-		s1Char = s1.charAt(n);
-		s2Char = s2.charAt(m);
+	// System.out.println("outer_cycle");
+	while ((n < s1.length()) && (m < s2.length())) {
+	    // System.out.print(" m: " + m + " n: " + n);
 
-		System.out.println(s2Char + " and " + s1Char);
-
-		if (s1Char != s2Char) {
-		    // No match.
-		    if (n < s1.length()) {
-			n++;
-			// break;
-		    } else {
-			break;
-		    }
-		} else {
-		    // Found a match.
-		    n++;
-		    m++;
-		}
+	    if ((n == s1.length()) || (m == s2.length())) {
+		break;
 	    }
-	// }
+
+	    s1Char = s1.charAt(n);
+	    s2Char = s2.charAt(m);
+
+	    // System.out.println(s2Char + " and " + s1Char);
+
+	    if (s1Char != s2Char) {
+		// No match.
+		n++;
+	    } else {
+		// Found a match.
+		n++;
+		m++;
+	    }
+	}
 
 	if (m >= s2.length()) {
 	    sTwoInSOne = false;
@@ -61,50 +59,44 @@ public class Main {
 
 	n = 0;
 	m = 0;
-	System.out.println("Look for " + s1 + " in " + s2);
+	// System.out.println("Look for " + s1 + " in " + s2);
 	// Search for s1 in s2.
-	//	for (; m < s1.length();) {
-	    System.out.println("outer_cycle");
-	    while ((n < s2.length()) && (m < s1.length())) {
-		System.out.print(" m: " + m + " n: " + n);
-		s1Char = s1.charAt(m);
-		s2Char = s2.charAt(n);
+	// System.out.println("outer_cycle");
+	while ((n < s2.length()) && (m < s1.length())) {
+	    // System.out.print(" m: " + m + " n: " + n);
 
-		System.out.println(s1Char + " and " + s2Char);
-
-		if (s1Char != s2Char) {
-		    // No match.
-		    if (n < s2.length()) {
-			n++;
-			// break;
-		    } else {
-			break;
-		    }
-		} else {
-		    // Found a match.
-		    n++;
-		    m++;
-		}
+	    if ((n == s2.length()) || (m == s1.length())) {
+		break;
 	    }
-	    //	}
 
-	System.out.println("PING3");
+	    s1Char = s1.charAt(m);
+	    s2Char = s2.charAt(n);
+
+	    // System.out.println(s1Char + " and " + s2Char);
+
+	    if (s1Char != s2Char) {
+		// No match.
+		n++;
+	    } else {
+		// Found a match.
+		n++;
+		m++;
+	    }
+	}
+
+	// System.out.println("PING3");
 	if (m >= s1.length()) {
 	    sOneInSTwo = false;
 	}
 
 	if (sOneInSTwo && sTwoInSOne) {
-	    System.out.println("PING");
-	    return PartialOrdering.EQUAL;
-	} else if (sOneInSTwo) {
-	    System.out.println("PING");
-	    return PartialOrdering.LESS;
-	} else if (sTwoInSOne) {
-	    System.out.println("PING");
-	    return PartialOrdering.GREATER;
-	} else {
-	    System.out.println("PING");
 	    return PartialOrdering.INCOMPARABLE;
+	} else if (sOneInSTwo) {
+	    return PartialOrdering.GREATER;
+	} else if (sTwoInSOne) {
+	    return PartialOrdering.LESS;
+	} else {
+	    return PartialOrdering.EQUAL;
 	}
     }
 
@@ -116,7 +108,9 @@ public class Main {
      */
     public static void main(String[] argse) {
 	System.out.println(subsequenceCompare("Hello World", "eloWrld"));
+	System.out.println(subsequenceCompare("eloWrld", "Hello World"));
 	System.out.println(subsequenceCompare("Hello World", "World Hello"));
+	System.out.println(subsequenceCompare("Hello", "Hello"));
     }
 }
 
